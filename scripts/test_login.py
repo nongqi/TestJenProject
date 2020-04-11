80994f2ee77c8a31b56d8938a49a5c525afef798
nonqi <771440224@qq.com>
2020-04-11 13:21:54 +0800

    first commit

---
+++ scripts/test_login.py
@@ -0,0 +1,12 @@
+class TestLogin:
+    def test_login_001(self):
+        print("001")
+        assert 0
+
+    def test_login_002(self):
+        print("002")
+        assert 1
+
+    def test_login_003(self):
+        print("003")
+        assert 1
